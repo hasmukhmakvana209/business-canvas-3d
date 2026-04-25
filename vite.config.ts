279b16ac265ffae351cc5f16ec2e98ec1a18f2d6
hasmukhmakvana209 <hasmukhmakvana209@gmail.com>
2026-04-25 13:35:20 +0530

    Add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import netlify from "@netlify/vite-plugin-tanstack-start";
+
+export default defineConfig({
+  cloudflare: false,
+  plugins: [
+    netlify(),
+  ],
+});
